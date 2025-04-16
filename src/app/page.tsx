c4385835df86a4904330c79a596017b809d245e0
Juan Toledo <juanmanueltoledo0487@gmail.com>
2025-04-16 11:17:22 +0000

    se ven dos footer y dos navbar  arregla eso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,16 @@
 import {ProductList} from '@/components/ProductList/ProductList';
 import {AboutUsSection} from '@/components/AboutUsSection/AboutUsSection';
 import {WhatWeDoSection} from '@/components/WhatWeDoSection/WhatWeDoSection';
-import {Footer} from '@/components/Footer/Footer';
-import {Navbar} from '@/components/Navbar/Navbar';
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
-
       <main className="flex-grow">
         <AboutUsSection/>
         <ProductList/>
         <WhatWeDoSection/>
       </main>
-
-      <Footer/>
     </div>
   );
 }
+
